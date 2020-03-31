0e50752c530bdb502839ea51dfadbcf0339e2c95
1714080902120 <1466645277@qq.com>
2020-03-31 16:30:41 +0800

    improve

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -2,18 +2,26 @@ import { QueryType, CreatePostDTO } from './dto/create-post.dto'
 import { BlogService } from './blog.service'
 import { Controller, Get, Res, HttpStatus, Query, Param, NotFoundException, Post, Body, Put, Delete } from '@nestjs/common'
 import { ValidateObjectId } from './shared/pipes/validate-object-id.pipes'
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
+
+
+@ApiTags('博客')
 @Controller('blog')
 export class BlogController {
   constructor(private blogService: BlogService) {}
 
+  @ApiTags('博客总数量')
   @Get('posts-length')
+  @ApiOperation({ summary: 'posts-length', description: '无参数'})
   async getLength(@Res() res) {
     const length = await this.blogService.getPostsLength()
     return res.status(HttpStatus.OK).json(length)
   }
 
   // 分页获取全部数据
+  @ApiTags('分页获取数据， 默认{ skip = 0, limit = 10 }')
   @Get('posts')
+  @ApiOperation({ summary: 'posts', description: '参数{ skip：页数默认为0， limit：每页数量默认为10 }'})
   async getPosts(@Res() res, @Query() query: QueryType) {
     let { skip, limit } = query
     const Posts = await this.blogService.getPosts(skip, limit)
@@ -21,7 +29,9 @@ export class BlogController {
   }
 
   // 模糊查询
+  @ApiTags('模糊查询,可以为任意字符串')
   @Get('blur-search')
+  @ApiOperation({ summary: 'blur-search', description: '参数为任意字符串'})
   async getPost(@Res() res, @Query() query) {
     let { data } = query
     const result = this.blogService.getPost(data)
@@ -29,7 +39,9 @@ export class BlogController {
   }
 
   // 查询通过ID
+  @ApiTags('通过ID查询')
   @Get('post/:postId')
+  @ApiOperation({ summary: 'post/:postId', description: '参数为postId'})
   async getPostById(@Res() res, @Param('postId', new ValidateObjectId()) postId) {
     const post = await this.blogService.getPostById(postId)
     if (!post) throw new NotFoundException('Post does not exist!')
@@ -37,7 +49,9 @@ export class BlogController {
   }
 
   // 增加
+  @ApiTags('添加')
   @Post('/post')
+  @ApiOperation({ summary: '/post', description: '参数需要满足Post模型'})
   async addPost(@Res() res, @Body() postDTO: CreatePostDTO) {
     const newPost = await this.blogService.addPost(postDTO);
     return res.status(HttpStatus.OK).json({
@@ -47,7 +61,9 @@ export class BlogController {
   }
 
   // 修改
+  @ApiTags('修改')
   @Put('/edit')
+  @ApiOperation({ summary: 'edit', description: '参数需要满足Post模型'})
   async editPost(
     @Res() res,
     @Query('postId', new ValidateObjectId()) postId,
@@ -62,7 +78,9 @@ export class BlogController {
   }
 
   // 删除
+  @ApiTags('删除')
   @Delete('/delete')
+  @ApiOperation({ summary: 'delete', description: '无参数'})
   async deletePost(@Res() res, @Query('postId', new ValidateObjectId()) postId) {
     const deletedPost = await this.blogService.deletePost(postId);
     if (!deletedPost) throw new NotFoundException('Post does not exist!');
